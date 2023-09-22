04c0b31f378edf101084e6903f9e7fab92c8134e
María Ruiz <mrp990305@gmail.com>
2023-09-22 18:58:39 +0200

    ejercicio terminado

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,31 +1,34 @@
 'use strict'
 
 function letters(cadena) {
-
+	return cadena.length;
 }
 
 function words(cadena) {
-
+	return cadena.split(' ').length;
 }
 
 function upperString(cadena) {
-
+	return cadena.toLocaleUpperCase();
 }
 
 function titleString(cadena) {
-
+	let array = cadena.split(' ');
+	let arrayNueva = array.map( cadena => cadena.charAt(0).toLocaleUpperCase() + cadena.slice(1));
+	return arrayNueva.join(' ');
 }
 
 function backwardsLetters(cadena) {
-
+	return cadena.split('').reverse().join('');
 }
 
 function backwardsWords(cadena) {
-
+	return cadena.split(' ').reverse().join(' ');
 }
 
 function palindrome(cadena) {
-
+	cadena = cadena.toLowerCase().replace(/ /g, '');
+	return cadena === cadena.split('').reverse().join('');
 }
 
 module.exports = {
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,20 @@
 'use strict'
 
+const { backwardsLetters, palindrome } = require("./functions");
 
+//const { letters, words } = require("./functions");
+
+let cadena = prompt("Introduce una frase: ");
+
+console.log('Frase: %s', cadena);
+console.log('%s letras y %s palabras', letters(cadena), words(cadena));
+console.log(upperString(cadena));
+console.log(titleString(cadena));
+console.log(backwardsLetters(cadena));
+console.log(backwardsWords(cadena));
+
+if (palindrome(cadena) == true) {
+    console.log('Sí es un palíndromo');
+} else {
+    console.log('No es un palíndromo');
+}
